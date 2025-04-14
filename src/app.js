d51069e2b6deee61651592785b5a6ff17a9aac5e
erazinkov <egorrazinkov@gmail.com>
2025-04-14 16:17:38 +0300

    added header

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 'use strict'
 
+import { MainView } from "./views/main/main";
+
 class App {
     routes = [
         {
             path: "",
-            view: null,
+            view: MainView,
         }
     ];
-    constructor() {}
+
+    appSate = {
+        favorites: [],
+    };
+
+    constructor() {
+        window.addEventListener('hashchange', this.route.bind(this));
+        this.route();
+    }
+
+    route() {
+        if (this.currentView) {
+            this.currentView.destroy();
+        }
+        const view = this.routes.find(r => r.path == location.hash).view;
+
+        this.currentView = new view(this.appSate);
+        this.currentView.render();
+    }
 }
 
 new App();
